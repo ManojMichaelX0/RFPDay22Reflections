7cae587969920977b43713332d0a56d7e083052d
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-23 21:39:16 +0530

    Uc 1 Reflections with test Cases

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionsUseCasesDay22;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSad()
+        {
+            string expected = "SAD";
+            string message = " I am in Sad Mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            string mood = moodAnalyser.AnalyserMood();
+
+            Assert.AreEqual(expected, mood);
+        }
+        [TestMethod]
+        public void GivenHappyShouldReturnHappy(string message)
+        {
+            string expected = "HAPPY";
+            string message1 = " I am in Happy Mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message1);
+
+            string mood = moodAnalyser.AnalyserMood();
+
+            Assert.AreEqual(expected, mood);
+        }
+        
+    }
+}
+
